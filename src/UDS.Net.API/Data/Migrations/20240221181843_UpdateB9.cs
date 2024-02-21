5c491b7d8599b09c4a37f6697413d287d1d0966c
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-02-21 13:20:57 -0500

    Update B9

---
+++ src/UDS.Net.API/Data/Migrations/20240221181843_UpdateB9.cs
@@ -0,0 +1,410 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UDS.Net.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateB9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ALSAGE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEAGE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEFPRED",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEFPREDX",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEMODE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEMODEX",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEOTHR",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEOTHRX",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEREMAGO",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "BEVHAGO",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "COGFLAGO",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "COGFPREX",
+                table: "tbl_B9s");
+
+            migrationBuilder.RenameColumn(
+                name: "PARKAGE",
+                table: "tbl_B9s",
+                newName: "PSYCHSYMIN");
+
+            migrationBuilder.RenameColumn(
+                name: "MOTREM",
+                table: "tbl_B9s",
+                newName: "PSYCHSYM");
+
+            migrationBuilder.RenameColumn(
+                name: "MOSLOW",
+                table: "tbl_B9s",
+                newName: "PERCHAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "MOMOPARK",
+                table: "tbl_B9s",
+                newName: "DECMOTIN");
+
+            migrationBuilder.RenameColumn(
+                name: "MOMODEX",
+                table: "tbl_B9s",
+                newName: "OTHSUBUSEX");
+
+            migrationBuilder.RenameColumn(
+                name: "MOMODE",
+                table: "tbl_B9s",
+                newName: "DECMOT");
+
+            migrationBuilder.RenameColumn(
+                name: "MOMOALS",
+                table: "tbl_B9s",
+                newName: "DECCOGIN");
+
+            migrationBuilder.RenameColumn(
+                name: "MOGAIT",
+                table: "tbl_B9s",
+                newName: "DECCOG");
+
+            migrationBuilder.RenameColumn(
+                name: "MOFRST",
+                table: "tbl_B9s",
+                newName: "COGAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "MOFALLS",
+                table: "tbl_B9s",
+                newName: "BEVPATT");
+
+            migrationBuilder.RenameColumn(
+                name: "MOAGE",
+                table: "tbl_B9s",
+                newName: "BESUBAB");
+
+            migrationBuilder.RenameColumn(
+                name: "LBDEVAL",
+                table: "tbl_B9s",
+                newName: "BEOBCOM");
+
+            migrationBuilder.RenameColumn(
+                name: "FTLDEVAL",
+                table: "tbl_B9s",
+                newName: "BEHAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "FRSTCHG",
+                table: "tbl_B9s",
+                newName: "BEEUPH");
+
+            migrationBuilder.RenameColumn(
+                name: "DECSUB",
+                table: "tbl_B9s",
+                newName: "BEEMPATH");
+
+            migrationBuilder.RenameColumn(
+                name: "DECCLMOT",
+                table: "tbl_B9s",
+                newName: "BEANGER");
+
+            migrationBuilder.RenameColumn(
+                name: "DECAGE",
+                table: "tbl_B9s",
+                newName: "BEAHSIMP");
+
+            migrationBuilder.RenameColumn(
+                name: "COURSE",
+                table: "tbl_B9s",
+                newName: "BEAHCOMP");
+
+            migrationBuilder.RenameColumn(
+                name: "COGFPRED",
+                table: "tbl_B9s",
+                newName: "BEAGGRS");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "DECCLCOG",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ALCUSE",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "COCAINEUSE",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DECCLIN",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OPIATEUSE",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OTHSUBUSE",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SEDUSE",
+                table: "tbl_B9s",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ALCUSE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "COCAINEUSE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "DECCLIN",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "OPIATEUSE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "OTHSUBUSE",
+                table: "tbl_B9s");
+
+            migrationBuilder.DropColumn(
+                name: "SEDUSE",
+                table: "tbl_B9s");
+
+            migrationBuilder.RenameColumn(
+                name: "PSYCHSYMIN",
+                table: "tbl_B9s",
+                newName: "PARKAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "PSYCHSYM",
+                table: "tbl_B9s",
+                newName: "MOTREM");
+
+            migrationBuilder.RenameColumn(
+                name: "PERCHAGE",
+                table: "tbl_B9s",
+                newName: "MOSLOW");
+
+            migrationBuilder.RenameColumn(
+                name: "OTHSUBUSEX",
+                table: "tbl_B9s",
+                newName: "MOMODEX");
+
+            migrationBuilder.RenameColumn(
+                name: "DECMOTIN",
+                table: "tbl_B9s",
+                newName: "MOMOPARK");
+
+            migrationBuilder.RenameColumn(
+                name: "DECMOT",
+                table: "tbl_B9s",
+                newName: "MOMODE");
+
+            migrationBuilder.RenameColumn(
+                name: "DECCOGIN",
+                table: "tbl_B9s",
+                newName: "MOMOALS");
+
+            migrationBuilder.RenameColumn(
+                name: "DECCOG",
+                table: "tbl_B9s",
+                newName: "MOGAIT");
+
+            migrationBuilder.RenameColumn(
+                name: "COGAGE",
+                table: "tbl_B9s",
+                newName: "MOFRST");
+
+            migrationBuilder.RenameColumn(
+                name: "BEVPATT",
+                table: "tbl_B9s",
+                newName: "MOFALLS");
+
+            migrationBuilder.RenameColumn(
+                name: "BESUBAB",
+                table: "tbl_B9s",
+                newName: "MOAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "BEOBCOM",
+                table: "tbl_B9s",
+                newName: "LBDEVAL");
+
+            migrationBuilder.RenameColumn(
+                name: "BEHAGE",
+                table: "tbl_B9s",
+                newName: "FTLDEVAL");
+
+            migrationBuilder.RenameColumn(
+                name: "BEEUPH",
+                table: "tbl_B9s",
+                newName: "FRSTCHG");
+
+            migrationBuilder.RenameColumn(
+                name: "BEEMPATH",
+                table: "tbl_B9s",
+                newName: "DECSUB");
+
+            migrationBuilder.RenameColumn(
+                name: "BEANGER",
+                table: "tbl_B9s",
+                newName: "DECCLMOT");
+
+            migrationBuilder.RenameColumn(
+                name: "BEAHSIMP",
+                table: "tbl_B9s",
+                newName: "DECAGE");
+
+            migrationBuilder.RenameColumn(
+                name: "BEAHCOMP",
+                table: "tbl_B9s",
+                newName: "COURSE");
+
+            migrationBuilder.RenameColumn(
+                name: "BEAGGRS",
+                table: "tbl_B9s",
+                newName: "COGFPRED");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DECCLCOG",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ALSAGE",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEAGE",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEFPRED",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BEFPREDX",
+                table: "tbl_B9s",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEMODE",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BEMODEX",
+                table: "tbl_B9s",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEOTHR",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BEOTHRX",
+                table: "tbl_B9s",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEREMAGO",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BEVHAGO",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "COGFLAGO",
+                table: "tbl_B9s",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "COGFPREX",
+                table: "tbl_B9s",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true);
+        }
+    }
+}
--- src/UDS.Net.API/Entities/B9.cs
+++ src/UDS.Net.API/Entities/B9.cs
@@ -9,72 +9,61 @@ namespace UDS.Net.API.Entities
     /// </summary>
     [Table("tbl_B9s")]
     public class B9 : Form
-	{
-        public int? DECSUB { get; set; }
-        public int? DECIN { get; set; }
-        public int? DECCLCOG { get; set; }
+    {
+        public int? DECCOG { get; set; }
+        public int? DECMOT { get; set; }
+        public int? PSYCHSYM { get; set; }
+        public int? DECCOGIN { get; set; }
+        public int? DECMOTIN { get; set; }
+        public int? PSYCHSYMIN { get; set; }
+        public bool? DECCLIN { get; set; }
+        public bool? DECCLCOG { get; set; }
         public int? COGMEM { get; set; }
+        public int? DECIN { get; set; }
         public int? COGORI { get; set; }
         public int? COGJUDG { get; set; }
         public int? COGLANG { get; set; }
         public int? COGVIS { get; set; }
         public int? COGATTN { get; set; }
         public int? COGFLUC { get; set; }
-        public int? COGFLAGO { get; set; }
         public int? COGOTHR { get; set; }
         [MaxLength(60)]
         public string? COGOTHRX { get; set; }
-        public int? COGFPRED { get; set; }
-        [MaxLength(60)]
-        public string? COGFPREX { get; set; }
+        public int? COGAGE { get; set; }
         public int? COGMODE { get; set; }
         [MaxLength(60)]
         public string? COGMODEX { get; set; }
-        public int? DECAGE { get; set; }
         public int? DECCLBE { get; set; }
         public int? BEAPATHY { get; set; }
         public int? BEDEP { get; set; }
-        public int? BEVHALL { get; set; }
-        public int? BEVWELL { get; set; }
-        public int? BEVHAGO { get; set; }
-        public int? BEAHALL { get; set; }
-        public int? BEDEL { get; set; }
-        public int? BEDISIN { get; set; }
+        public int? BEANX { get; set; }
+        public int? BEEUPH { get; set; }
         public int? BEIRRIT { get; set; }
         public int? BEAGIT { get; set; }
+        public int? BEHAGE { get; set; }
+        public int? BEVHALL { get; set; }
+        public int? BEVPATT { get; set; }
+        public int? BEVWELL { get; set; }
+        public int? BEAHALL { get; set; }
+        public int? BEAHSIMP { get; set; }
+        public int? BEAHCOMP { get; set; }
+        public int? BEDEL { get; set; }
+        public int? BEAGGRS { get; set; }
+        public int? BEDISIN { get; set; }
         public int? BEPERCH { get; set; }
+        public int? BEEMPATH { get; set; }
+        public int? BEOBCOM { get; set; }
+        public int? BEANGER { get; set; }
+        public int? BESUBAB { get; set; }
+        public bool? ALCUSE { get; set; }
+        public bool? SEDUSE { get; set; }
+        public bool? OPIATEUSE { get; set; }
+        public bool? COCAINEUSE { get; set; }
+        public bool? OTHSUBUSE { get; set; }
+        [MaxLength(60)]
+        public string? OTHSUBUSEX { get; set; }
+        public int? PERCHAGE { get; set; }
         public int? BEREM { get; set; }
-        public int? BEREMAGO { get; set; }
-        public int? BEANX { get; set; }
-        public int? BEOTHR { get; set; }
-        [MaxLength(60)]
-        public string? BEOTHRX { get; set; }
-        public int? BEFPRED { get; set; }
-        [MaxLength(60)]
-        public string? BEFPREDX { get; set; }
-        public int? BEMODE { get; set; }
-        [MaxLength(60)]
-        public string? BEMODEX { get; set; }
-        public int? BEAGE { get; set; }
-        public int? DECCLMOT { get; set; }
-        public int? MOGAIT { get; set; }
-        public int? MOFALLS { get; set; }
-        public int? MOTREM { get; set; }
-        public int? MOSLOW { get; set; }
-        public int? MOFRST { get; set; }
-        public int? MOMODE { get; set; }
-        [MaxLength(60)]
-        public string? MOMODEX { get; set; }
-        public int? MOMOPARK { get; set; }
-        public int? PARKAGE { get; set; }
-        public int? MOMOALS { get; set; }
-        public int? ALSAGE { get; set; }
-        public int? MOAGE { get; set; }
-        public int? COURSE { get; set; }
-        public int? FRSTCHG { get; set; }
-        public int? LBDEVAL { get; set; }
-        public int? FTLDEVAL { get; set; }
-
     }
 }
 
