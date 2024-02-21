cff465ccd6697b999e5d9cb4a101ff1ecd27c2d8
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-02-21 12:56:31 -0500

    Update B8

--- src/UDS.Net.Dto/B8Dto.cs
+++ src/UDS.Net.Dto/B8Dto.cs
@@ -3,49 +3,44 @@ namespace UDS.Net.Dto
 {
     public class B8Dto : FormDto
     {
-        public int? NORMEXAM { get; set; }
+        public int? NEUREXAM { get; set; }
+        public bool? NORMNREXAM { get; set; }
         public int? PARKSIGN { get; set; }
-        public int? RESTTRL { get; set; }
-        public int? RESTTRR { get; set; }
-        public int? SLOWINGL { get; set; }
-        public int? SLOWINGR { get; set; }
-        public int? RIGIDL { get; set; }
-        public int? RIGIDR { get; set; }
-        public int? BRADY { get; set; }
-        public int? PARKGAIT { get; set; }
+        public int? SLOWINGFM { get; set; }
+        public int? TREMREST { get; set; }
+        public int? TREMPOST { get; set; }
+        public int? TREMKINE { get; set; }
+        public int? RIGIDARM { get; set; }
+        public int? RIGIDLEG { get; set; }
+        public int? DYSTARM { get; set; }
+        public int? DYSTLEG { get; set; }
+        public int? CHOREA { get; set; }
+        public int? AMPMOTOR { get; set; }
+        public int? AXIALRIG { get; set; }
         public int? POSTINST { get; set; }
-        public int? CVDSIGNS { get; set; }
-        public int? CORTDEF { get; set; }
-        public int? SIVDFIND { get; set; }
-        public int? CVDMOTL { get; set; }
-        public int? CVDMOTR { get; set; }
-        public int? CORTVISL { get; set; }
-        public int? CORTVISR { get; set; }
-        public int? SOMATL { get; set; }
-        public int? SOMATR { get; set; }
-        public int? POSTCORT { get; set; }
-        public int? PSPCBS { get; set; }
-        public int? EYEPSP { get; set; }
-        public int? DYSPSP { get; set; }
-        public int? AXIALPSP { get; set; }
-        public int? GAITPSP { get; set; }
+        public int? MASKING { get; set; }
+        public int? STOOPED { get; set; }
+        public int? OTHERSIGN { get; set; }
+        public int? LIMBAPRAX { get; set; }
+        public int? UMNDIST { get; set; }
+        public int? LMNDIST { get; set; }
+        public int? VFIELDCUT { get; set; }
+        public int? LIMBATAX { get; set; }
+        public int? MYOCLON { get; set; }
+        public int? UNISOMATO { get; set; }
+        public int? APHASIA { get; set; }
+        public int? ALIENLIMB { get; set; }
+        public int? HSPATNEG { get; set; }
+        public int? PSPOAGNO { get; set; }
+        public int? SMTAGNO { get; set; }
+        public int? OPTICATAX { get; set; }
+        public int? APRAXGAZE { get; set; }
+        public int? VHGAZEPAL { get; set; }
+        public int? DYSARTH { get; set; }
         public int? APRAXSP { get; set; }
-        public int? APRAXL { get; set; }
-        public int? APRAXR { get; set; }
-        public int? CORTSENL { get; set; }
-        public int? CORTSENR { get; set; }
-        public int? ATAXL { get; set; }
-        public int? ATAXR { get; set; }
-        public int? ALIENLML { get; set; }
-        public int? ALIENLMR { get; set; }
-        public int? DYSTONL { get; set; }
-        public int? DYSTONR { get; set; }
-        public int? MYOCLLT { get; set; }
-        public int? MYOCLRT { get; set; }
-        public int? ALSFIND { get; set; }
-        public int? GAITNPH { get; set; }
-        public int? OTHNEUR { get; set; }
-        public string OTHNEURX { get; set; }
+        public int? GAITABN { get; set; }
+        public int? GAITFIND { get; set; }
+        public string GAITOTHRX { get; set; }
     }
 }
 
