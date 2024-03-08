f0588e2cca22571f2a4ca4c692a9508d748f30ed
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-03-08 13:33:54 -0500

    Add migration

--- src/UDS.Net.API/Data/Migrations/20240308181713_AddD1a.Designer.cs
+++ src/UDS.Net.API/Data/Migrations/20240308181713_AddD1a.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UDS.Net.API.Data;
 
@@ -11,9 +12,11 @@ using UDS.Net.API.Data;
 namespace UDS.Net.API.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240308181713_AddD1a")]
+    partial class AddD1a
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
