279d66ffe518e40f2a57c79b4e5eadeed0c0ef97
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-07-24 21:05:33 -0400

    adds milestonetype migration

---
+++ src/UDS.Net.API/Data/Migrations/20240725010419_MilestoneType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UDS.Net.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MilestoneType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MILESTONETYPE",
+                table: "tbl_M1s",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MILESTONETYPE",
+                table: "tbl_M1s");
+        }
+    }
+}
