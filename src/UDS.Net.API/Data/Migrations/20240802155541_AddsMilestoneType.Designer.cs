42dd55232df22f2acf7ad2c26f0b5299c46f8e3d
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-08-02 11:57:11 -0400

    Reapply migration for MILESTONETYPE

--- src/UDS.Net.API/Data/Migrations/20240802155541_AddsMilestoneType.Designer.cs
+++ src/UDS.Net.API/Data/Migrations/20240802155541_AddsMilestoneType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UDS.Net.API.Data;
 
@@ -11,9 +12,11 @@ using UDS.Net.API.Data;
 namespace UDS.Net.API.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240802155541_AddsMilestoneType")]
+    partial class AddsMilestoneType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
