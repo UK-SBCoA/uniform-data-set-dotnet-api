55f8390298eaada17a5294fc02760c6bf21b9169
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-11-27 11:28:49 -0500

    Add entity class and seeds txt file

---
+++ src/UDS.Net.API/Entities/CountryCodesLookup.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UDS.Net.API.Entities
+{
+    public class CountryCodesLookup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public string? Code { get; set; }
+
+        public string? Country { get; set; }
+    }
+}
